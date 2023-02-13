2c407f0f9f9914483b0c854278684f513fb7077f
TanguyDessoly <pro.tanguydessoly@gmail.com>
2023-02-13 19:34:36 +0100

    FIGHT

--- app/routers/pokemons.py
+++ app/routers/pokemons.py
@@ -1,8 +1,10 @@
+from random import randint
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter,  Depends
 from app import actions, schemas
 from app.utils.utils import get_db
+from ..utils.pokeapi import battle_pokemon, get_pokemon_data
 
 router = APIRouter()
 
@@ -14,4 +16,24 @@ def get_pokemons(skip: int = 0, limit: int = 100, database: Session = Depends(ge
         Default limit is 100
     """
     pokemons = actions.get_pokemons(database, skip=skip, limit=limit)
+    return pokemons
+
+@router.get("/fight/{premierPokemon}/{secondPokemon}")
+def pokemons_battle(premierPokemon: int, secondPokemon: int):
+    """
+        Return the winner of a fight between two pokemons
+    """
+    return battle_pokemon(premierPokemon, secondPokemon)
+
+
+@router.get("/random")
+def pokemons_random():
+    """
+        Return 3 random pokemons
+    """
+    pokemons: list[dict, dict, dict] = []
+    while len(pokemons) != 3:
+        pokemon = get_pokemon_data(randint(1, 1279))
+        if pokemon not in pokemons and pokemon is not None:
+            pokemons.append(pokemon)
     return pokemons
--- app/utils/pokeapi.py
+++ app/utils/pokeapi.py
@@ -13,26 +13,40 @@ def get_pokemon_stats(api_id):
     """
         Get pokemon stats from the API pokeapi
     """
-    return False
+    return get_pokemon_data(api_id)["stats"]
 
 def get_pokemon_data(api_id):
     """
         Get data of pokemon name from the API pokeapi
     """
-    return requests.get(f"{base_url}/pokemon/{api_id}", timeout=10).json()
+    result = requests.get(f"{base_url}/pokemon/{api_id}", timeout=10)
+    if result.status_code > 199 and result.status_code < 300:
+        return result.json()
+    return None
 
-
-def battle_pokemon(first_api_id, second_api_id):
+def battle_pokemon(first_api_id, second_api_id) :
     """
         Do battle between 2 pokemons
     """
     premierPokemon = get_pokemon_data(first_api_id)
     secondPokemon = get_pokemon_data(second_api_id)
-    battle_result = 0
-    return premierPokemon if battle_result > 0 else secondPokemon if battle_result < 0 else {'winner': 'draw'}
+    battle_result = 0    
+    if premierPokemon and secondPokemon:
+        battle_result = battle_compare_stats(premierPokemon["stats"], secondPokemon["stats"])
+        if battle_result > 0:
+            return {"Pokemon gagnant":premierPokemon["name"].capitalize()}
+        if battle_result < 0:
+            return {"Pokemon gagnant":secondPokemon["name"].capitalize()}
+        else:
+            return {"Egalité"}
+    return None
 
 
-def battle_compare_stats(first_pokemon_stats, second_pokemon_stats):
+def battle_compare_stats(premierPokemon, secondPokemon):
     """
         Compare given stat between two pokemons
     """
+    result = 0
+    for i, stats in enumerate(premierPokemon):
+        result += stats["base_stat"] - secondPokemon[i]["base_stat"]
+    return result
